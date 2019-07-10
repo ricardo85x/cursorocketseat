8387869a779db84b090e00775753d26007e8ca0e
Ricardo <ricardo85x@gmail.com>
2019-07-09 21:55:07 -0300

    modulo 6 - Buscando dados da API

--- BootCamp/bootcamp2019/modulo6/src/pages/User/index.js
+++ BootCamp/bootcamp2019/modulo6/src/pages/User/index.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View, Text } from 'react-native';
-
+import api from '../../services/api';
 // import { Container } from './styles';
 
-export default function User({ navigation }) {
-    console.tron.log(navigation.getParam('user'));
-    return (
-        <View>
-            <Text>Ola User</Text>
-        </View>
-    );
+export default class User extends Component {
+    static navigationOptions = ({ navigation }) => ({
+        title: navigation.getParam('user').name,
+    });
+
+    state = {
+        stars: [],
+    };
+
+    static propTypes = {
+        navigation: PropTypes.shape({
+            getParam: PropTypes.func,
+        }).isRequired,
+    };
+
+    async componentDidMount() {
+        const { navigation } = this.props;
+        const user = navigation.getParam('user');
+        const { stars } = this.state;
+
+        const response = await api.get(`/users/${user.login}/starred`);
+
+        this.setState({ stars: [...stars, response.data] });
+    }
+
+    render() {
+        const { stars } = this.state;
+        return (
+            <View>
+                <Text>Ola User</Text>
+            </View>
+        );
+    }
 }
