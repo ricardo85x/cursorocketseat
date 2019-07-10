751b49b1f7652a4874bc9562632fffc03020bd0d
Ricardo <ricardo85x@gmail.com>
2019-07-09 22:50:03 -0300

    modulo 6 - Listando favoritos

--- BootCamp/bootcamp2019/modulo6/src/pages/User/index.js
+++ BootCamp/bootcamp2019/modulo6/src/pages/User/index.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
+// import { View, Text } from 'react-native';
 import api from '../../services/api';
-// import { Container } from './styles';
+import {
+    Container,
+    Header,
+    Avatar,
+    Name,
+    Bio,
+    Stars,
+    Stared,
+    OwnerAvatar,
+    Info,
+    Title,
+    Author,
+} from './styles';
 
 export default class User extends Component {
     static navigationOptions = ({ navigation }) => ({
@@ -22,19 +34,46 @@ export default class User extends Component {
     async componentDidMount() {
         const { navigation } = this.props;
         const user = navigation.getParam('user');
-        const { stars } = this.state;
 
         const response = await api.get(`/users/${user.login}/starred`);
 
-        this.setState({ stars: [...stars, response.data] });
+        console.tron.log(response.data);
+
+        this.setState({ stars: response.data });
     }
 
     render() {
+        const { navigation } = this.props;
         const { stars } = this.state;
+
+        console.tron.log(stars);
+
+        const user = navigation.getParam('user');
+
         return (
-            <View>
-                <Text>Ola User</Text>
-            </View>
+            <Container>
+                <Header>
+                    <Avatar source={{ uri: user.avatar }} />
+                    <Name>{user.name}</Name>
+                    <Bio>{user.bio} </Bio>
+                </Header>
+
+                <Stars
+                    data={stars}
+                    keyExtractor={star => String(star.id)}
+                    renderItem={({ item }) => (
+                        <Stared>
+                            <OwnerAvatar
+                                source={{ uri: item.owner.avatar_url }}
+                            />
+                            <Info>
+                                <Title>{item.name}</Title>
+                                <Author>{item.owner.login}</Author>
+                            </Info>
+                        </Stared>
+                    )}
+                />
+            </Container>
         );
     }
 }
