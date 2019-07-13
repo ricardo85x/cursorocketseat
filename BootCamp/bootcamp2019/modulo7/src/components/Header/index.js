3d8346eff8cda39cbfc92adb24ec15b9a45414e3
Ricardo <ricardo85x@gmail.com>
2019-07-13 12:49:06 -0300

    modulo 7 - Adicionando ao carrinho e proptypes

--- BootCamp/bootcamp2019/modulo7/src/components/Header/index.js
+++ BootCamp/bootcamp2019/modulo7/src/components/Header/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ProTypes from 'prop-types';
+
+import { connect } from 'react-redux';
 
 import { MdShoppingBasket } from 'react-icons/md'; // md = material design
 import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
-export default function Header() {
+function Header({ cartSize }) {
     return (
         <Container>
             <Link to="/">
@@ -16,10 +19,18 @@ export default function Header() {
             <Cart to="/cart">
                 <div>
                     <strong>Meu carrinho</strong>
-                    <span>3 itens</span>
+                    <span>{cartSize} itens</span>
                 </div>
                 <MdShoppingBasket size={36} color="#FFF" />
             </Cart>
         </Container>
     );
 }
+
+Header.propTypes = {
+    cartSize: ProTypes.number.isRequired,
+};
+export default connect(state => ({
+    carrinho: state.cart,
+    cartSize: state.cart.length,
+}))(Header);
