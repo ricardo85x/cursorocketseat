8c601e5b015ab5c9ef0ad65b70646eee9db969d7
ricardo <ricardo85x@gmail.com>
2019-07-22 22:05:26 -0300

    Hook useState

--- BootCamp/bootcamp2019/modulo8/src/App.js
+++ BootCamp/bootcamp2019/modulo8/src/App.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Ola mundo</h1>;
+  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+        name=""
+        id=""
+      />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
