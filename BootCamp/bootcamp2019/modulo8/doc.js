9746531b4ed48a089b8cb5cc1498769fb5274b18
ricardo <ricardo@pop-os.localdomain>
2019-07-22 21:41:27 -0300

    Configurando estrutura

---
+++ BootCamp/bootcamp2019/modulo8/doc.js
@@ -0,0 +1,11 @@
+export const doc = {
+  modulo: {
+    name: 'eslint-plugin-react-hooks',
+    dev: true,
+    desc: `
+      avisa se estamos usando o 
+      react hooks de forma errada
+      ps: adicionar no .eslintrc
+    `,
+  },
+};
