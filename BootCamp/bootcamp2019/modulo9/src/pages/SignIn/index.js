78c4543323e760fe363bdd1479ac8071fd730183
ricardo <ricardo85x@gmail.com>
2019-08-03 23:03:03 -0300

    modulo9 - Estilizaçao da autenticacao

--- BootCamp/bootcamp2019/modulo9/src/pages/SignIn/index.js
+++ BootCamp/bootcamp2019/modulo9/src/pages/SignIn/index.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "~/assets/logo.svg";
 
 export default function SignIn() {
-  return <h1>Hello SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Entrar</button>
+
+        <Link to="/register">Novo cadastro</Link>
+      </form>
+    </>
+  );
 }
--- BootCamp/bootcamp2019/modulo9/src/pages/SignUp/index.js
+++ BootCamp/bootcamp2019/modulo9/src/pages/SignUp/index.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "~/assets/logo.svg";
 
 export default function SignUp() {
-  return <div>Hello SignUp</div>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Criar conta</button>
+
+        <Link to="/">Ja tenho login</Link>
+      </form>
+    </>
+  );
 }
