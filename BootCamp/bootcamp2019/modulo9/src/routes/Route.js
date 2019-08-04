b35c4e26d182ec1c656390ccae4829301ce54672
ricardo <ricardo85x@gmail.com>
2019-08-03 21:43:30 -0300

    modulo9 - Layouts por pagina

--- BootCamp/bootcamp2019/modulo9/src/routes/Route.js
+++ BootCamp/bootcamp2019/modulo9/src/routes/Route.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
 
+import AuthLayout from "../pages/_layouts/auth";
+import DefaultLayout from "../pages/_layouts/default";
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const signed = true;
 
   if (!signed && isPrivate) {
     console.tron.log("nao estou logado e sou private");
@@ -23,7 +26,18 @@ export default function RouteWrapper({
   console.tron.log("signed", signed);
   console.tron.log("isPrivate", isPrivate);
 
-  return <Route {...rest} component={Component} />;
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
