a7822e007dbe0742d9969a56bd174b817e0fb3f1
ricardo <ricardo85x@gmail.com>
2019-08-03 22:20:34 -0300

    modulo9 - Utilizando Root Import

---
+++ BootCamp/bootcamp2019/modulo9/doc.js
@@ -0,0 +1,23 @@
+export default {
+  modules: [
+    {
+      name: "customize-cra e react-app-rewired",
+      desc:
+        "permite modificar configurações do babel em um app criado com o create react app",
+      devel: true
+    },
+    {
+      name: "babel-plugin-root-import",
+      desc: `permite modificar os import dos arquivos 
+            dentro do app sem usar aqueles .. para dizer
+            que esta no nivel anterior, etc`,
+      devel: true
+    },
+    {
+      name: "eslint-import-resolver-babel-plugin-root-import",
+      desc:
+        "para o eslint não reclamar do ~ no import dos aruivos com o plugin root-import",
+      devel: true
+    }
+  ]
+};
