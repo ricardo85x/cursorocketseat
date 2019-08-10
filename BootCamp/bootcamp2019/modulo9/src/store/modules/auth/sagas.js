c4141050c79cc348e002baf827b159ba3b3b57c0
ricardo <ricardo85x@gmail.com>
2019-08-09 21:18:13 -0300

    modulo9 - Exibindo toasts

--- BootCamp/bootcamp2019/modulo9/src/store/modules/auth/sagas.js
+++ BootCamp/bootcamp2019/modulo9/src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from "redux-saga/effects";
+import { toast } from "react-toastify";
 
 import history from "~/services/history";
 import api from "~/services/api";
@@ -15,7 +16,7 @@ export function* signIn({ payload }) {
     const { user, token } = response.data;
 
     if (!user.provider) {
-      console.tron.error("usuario não é prestador de serviço");
+      toast.error("usuario não é prestador de serviço");
       return;
     }
 
@@ -26,6 +27,7 @@ export function* signIn({ payload }) {
     history.push("/dashboard");
   } catch (e) {
     yield put(signFailure());
+    toast.error("Falha na autenticação, verifique seus dados");
   }
 }
 
