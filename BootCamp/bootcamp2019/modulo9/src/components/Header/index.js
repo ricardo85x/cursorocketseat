6db88f5ffc6e12678a371a27548acd572e5de084
ricardo <ricardo85x@gmail.com>
2019-08-10 23:20:04 -0300

    modulo9 - Logout da aplicaçao

--- BootCamp/bootcamp2019/modulo9/src/components/Header/index.js
+++ BootCamp/bootcamp2019/modulo9/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Container, Content, Profile } from "./styles";
 
@@ -6,6 +7,7 @@ import Notifications from "../Notifications";
 import logo from "~/assets/logo-purple.svg";
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
   return (
     <Container>
       <Content>
@@ -18,12 +20,15 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong> Ricardo Ferreira </strong>
+              <strong> {profile.name} </strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Ricardo Ferreira"
+              src={
+                profile.avatar.url ||
+                "https://api.adorable.io/avatars/50/abott@adorable.png"
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
