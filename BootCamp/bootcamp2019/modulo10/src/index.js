b824bc7aec1902faef076101f4c77ecaa7a9c4e5
ricardo <ricardo85x@gmail.com>
2019-08-17 20:12:49 -0300

    modulo10 - 1 Estrura configurada + ESLINT

---
+++ BootCamp/bootcamp2019/modulo10/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StatusBar} from 'react-native';
+
+const App = () => (
+    <>
+        <StatusBar barStyle="dark-content" />
+        <View>
+            <Text>Ola mundo cruel </Text>
+        </View>
+    </>
+);
+
+export default App;
