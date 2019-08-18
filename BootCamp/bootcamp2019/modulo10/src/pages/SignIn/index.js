454c3bac3c5ec4860764ec4d25eb43857074f338
ricardo <ricardo85x@gmail.com>
2019-08-18 12:21:10 -0300

    modulo10 - 6 Pagina SignIn

--- BootCamp/bootcamp2019/modulo10/src/pages/SignIn/index.js
+++ BootCamp/bootcamp2019/modulo10/src/pages/SignIn/index.js
@@ -1,25 +1,49 @@
 import React from 'react';
 
-import {Text} from 'react-native';
+import {Image} from 'react-native';
 
-import Input from '~/components/Input';
-import Button from '~/components/Button';
+import logo from '~/assets/logo.png';
+
+// import Input from '~/components/Input';
+// import Button from '~/components/Button';
 import Background from '~/components/Background';
 
-import {Container} from './styles';
+import {
+    Container,
+    Form,
+    FormInput,
+    SubmitButton,
+    SignLink,
+    SignLinkText,
+} from './styles';
 
 export default function SignIn() {
     return (
         <Background>
             <Container>
-                <Text>Ola SignIn</Text>
-                <Input
-                    icon="call"
-                    placeholder="digite seu nome"
-                    style={{marginTop: 30}}
-                />
+                <Image source={logo} />
 
-                <Button>Entrar</Button>
+                <Form>
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-address"
+                        autoCorrect={false}
+                        autoCaptalize="none"
+                        placeholder="Digite seu email"
+                    />
+
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Digite sua senha"
+                    />
+
+                    <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+                </Form>
+
+                <SignLink onPress={() => {}}>
+                    <SignLinkText>Criar conta gratuita!</SignLinkText>
+                </SignLink>
             </Container>
         </Background>
     );
