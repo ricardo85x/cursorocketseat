98013e20ead6e3f4e9a0cfe38daeb263e5547aec
ricardo <ricardo85x@gmail.com>
2019-08-19 22:45:11 -0300

    modulo10 - 15 Lista de agendamentos

--- BootCamp/bootcamp2019/modulo10/src/pages/Dashboard/styles.js
+++ BootCamp/bootcamp2019/modulo10/src/pages/Dashboard/styles.js
@@ -1,3 +1,19 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.SafeAreaView`
+    /* deixa o conteudo em baixo do topete do iphone(SafeAreaView)*/
+    flex: 1;
+`;
+
+export const Title = styled.Text`
+    font-size: 20px;
+    color: #fff;
+    font-weight: bold;
+    align-self: center;
+    margin-top: 30px;
+`;
+
+export const List = styled.FlatList.attrs({
+    contentContainerStyle: {padding: 30},
+    showsVerticalScrollIndicator: false,
+})``;
