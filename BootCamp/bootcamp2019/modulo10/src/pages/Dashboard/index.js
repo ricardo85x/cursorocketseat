d049ae8f1c480387f866082407256a1db489e7e8
ricardo <ricardo85x@gmail.com>
2019-08-20 22:03:21 -0300

    modulo10 - 16 Agendamentos da API

--- BootCamp/bootcamp2019/modulo10/src/pages/Dashboard/index.js
+++ BootCamp/bootcamp2019/modulo10/src/pages/Dashboard/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Alert} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import PropTypes from 'prop-types';
+import api from '~/services/api';
+
 import Background from '~/components/Background';
 
 import Appointment from '~/components/Appointment';
@@ -14,15 +16,44 @@ import {Container, Title, List} from './styles';
 const data = [1, 2, 3, 4, 5];
 
 export default function Dashboard() {
+    const [appointments, setAppointments] = useState([]);
+    useEffect(() => {
+        async function loadAppointments() {
+            const response = await api.get('/appointments');
+
+            setAppointments(response.data);
+        }
+        loadAppointments();
+    }, []);
+
+    async function handleCancel({item: {id}}) {
+        const response = await api.delete(`/appointments/${id}`);
+
+        Alert.alert('Agendamento cancelado!', 'vedade');
+
+        setAppointments(
+            appointments.map(item =>
+                item.id === id
+                    ? {...item, canceled_at: response.data.canceled_at}
+                    : item,
+            ),
+        );
+    }
+
     return (
         <Background>
             <Container>
                 <Title>Agendamentos</Title>
 
                 <List
-                    data={data}
-                    keyExtractor={item => String(item)}
-                    renderItem={item => <Appointment data={item} />}
+                    data={appointments}
+                    keyExtractor={item => String(item.id)}
+                    renderItem={item => (
+                        <Appointment
+                            onCancel={() => handleCancel(item)}
+                            data={item}
+                        />
+                    )}
                 />
             </Container>
         </Background>
