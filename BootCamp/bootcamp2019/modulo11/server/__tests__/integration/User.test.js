88de8a972cddb77a7a4afc2211c5c7a50878b533
ricardo <ricardo85x@gmail.com>
2019-09-08 15:42:12 -0300

    Email duplicado

--- BootCamp/bootcamp2019/modulo11/server/__tests__/integration/User.test.js
+++ BootCamp/bootcamp2019/modulo11/server/__tests__/integration/User.test.js
@@ -1,8 +1,13 @@
 import request from 'supertest';
 
 import app from '../../src/app';
+import truncate from '../util/truncate';
 
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -31,6 +36,6 @@ describe('User', () => {
         password_hash: '123456',
       });
 
-    expect(responseDuplicate.body).toHaveProperty('error');
+    expect(responseDuplicate.status).toBe(400);
   });
 });
--- BootCamp/bootcamp2019/modulo11/server/src/app/controllers/UserController.js
+++ BootCamp/bootcamp2019/modulo11/server/src/app/controllers/UserController.js
@@ -2,6 +2,13 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const { email } = req.body;
+
+    const user_exists = await User.findOne({ where: { email } });
+
+    if (user_exists) {
+      return res.status(400).json({ error: 'usuario existe' });
+    }
     const user = await User.create(req.body);
     return res.json(user);
   }
