8f9b3cb38d1b8814130c61c5102dec1a4f604640
ricardo <ricardo85x@gmail.com>
2019-09-08 16:03:27 -0300

    Criptografia de senha e factory girl

--- BootCamp/bootcamp2019/modulo11/server/__tests__/integration/User.test.js
+++ BootCamp/bootcamp2019/modulo11/server/__tests__/integration/User.test.js
@@ -1,6 +1,9 @@
 import request from 'supertest';
-
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
+import factory from '../factories';
+
+// import User from '../../src/app/models/User';
 import truncate from '../util/truncate';
 
 describe('User', () => {
@@ -8,33 +11,35 @@ describe('User', () => {
     await truncate();
   });
 
+  it('should encrypt user password then new user created', async () => {
+    const user = await factory.create('User', {
+      password: '123456',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
+  });
+
   it('should be able to register', async () => {
+    const user = await factory.attrs('User');
+
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Ricardo God',
-        email: 'ricardo@a.com',
-        password_hash: '123456',
-      });
+      .send(user);
 
     expect(response.body).toHaveProperty('id');
   });
   it('should return error if user alread exists', async () => {
+    const user = await factory.attrs('User');
+
     await request(app)
       .post('/users')
-      .send({
-        name: 'Ricardo God',
-        email: 'ricardo@a.com',
-        password_hash: '123456',
-      });
+      .send(user);
 
     const responseDuplicate = await request(app)
       .post('/users')
-      .send({
-        name: 'Ricardo God',
-        email: 'ricardo@a.com',
-        password_hash: '123456',
-      });
+      .send(user);
 
     expect(responseDuplicate.status).toBe(400);
   });
