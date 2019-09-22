b8513aed8b128123386f435431fe7b13a9c30b8a
ricardo <ricardo85x@gmail.com>
2019-09-22 11:23:55 -0300

    modulo12

---
+++ BootCamp/bootcamp2019/modulo12/doc.js
@@ -0,0 +1,10 @@
+export default {
+    modulos: [
+        {
+            name: 'react-app-rewired',
+            dev: true,
+            desc: 'possibilita alterar alguns configurações da aplicação como o jest do react',
+            conf: 'no package.json troque o react-scripts por react-app-rewired no start, build, test'
+        }
+    ]   
+}
---
+++ BootCamp/bootcamp2019/modulo12/src/App.test.js
@@ -0,0 +1,3 @@
+test('exemple', () => {
+    expect(1 + 1).toBe(2)
+})
