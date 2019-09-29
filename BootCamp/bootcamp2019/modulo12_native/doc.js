cb6c50c92495e8fd1d1559f48f6b3d525c4c5a4c
ricardo <ricardo85x@gmail.com>
2019-09-29 15:09:19 -0300

    modulo12 - 11 Testes no React Native

---
+++ BootCamp/bootcamp2019/modulo12_native/doc.js
@@ -0,0 +1,17 @@
+const doc = {
+  modules: [
+    {
+      name: '@types/jest',
+      dev: true,
+      desc: 'intelecense de testes',
+    },
+    {
+      name: '@testing-library/jest-native',
+      dev: true,
+      desc:
+        'adiciona mais funcionalidado no jest no react native, como pegar o valor do input',
+    },
+  ],
+};
+
+export default doc;
