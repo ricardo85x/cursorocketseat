f5c2341e9981da5b78fcbbe0137cfed462d0955e
ricardo <ricardo.egss@gmail.com>
2019-10-06 16:05:34 -0300

    modulo13 deploy - Enviando notificacoes push

--- BootCamp/bootcamp2019/modulo13_deploy/src/index.js
+++ BootCamp/bootcamp2019/modulo13_deploy/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import CodePush from 'react-native-code-push'
+import OneSignal from 'react-native-onesignal'
 import {PersistGate} from 'redux-persist/integration/react';
 import {Provider} from 'react-redux';
 import {StatusBar} from 'react-native';
@@ -11,15 +12,55 @@ import {store, persistor} from '~/store';
 
 import App from '~/App';
 
-const Index = () => (
-    <Provider store={store}>
-        <PersistGate persistor={persistor}>
-            {/* <StatusBar barStyle="light-content" backgroundColor="#22202C" /> */}
-            <StatusBar hidden />
-            <App />
-        </PersistGate>
-    </Provider>
-);
+
+class Index  extends Component {
+
+    constructor(props) {
+        super(props)
+
+        OneSignal.init('b01cf639-85a7-46f3-acf7-1bb0a4ec42db')
+
+        OneSignal.addEventListener('received', this.onReceived)
+        OneSignal.addEventListener('opened', this.onOpened)
+        OneSignal.addEventListener('ids', this.onIds)
+    }
+
+
+    componentWillUnmount(){
+        OneSignal.addEventListener('received', this.onReceived)
+        OneSignal.addEventListener('opened', this.onOpened)
+        OneSignal.addEventListener('ids', this.onIds)
+    }
+
+    onReceived = (data) => {
+        console.log(data)
+    }
+    onOpened = (notification)  => {
+
+    }
+
+    onIds = (id) => {
+
+    }
+
+
+    render (){
+
+        return (
+
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                {/* <StatusBar barStyle="light-content" backgroundColor="#22202C" /> */}
+                <StatusBar hidden />
+                <App />
+            </PersistGate>
+        </Provider>
+
+        )
+    }
+}
+    
+
 
 
 
